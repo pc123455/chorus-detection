4b10ae03785b7ad70d230753b467d538daf5a091
Weiyao.Xue <wyxue_std@163.com>
2017-03-23 15:26:27 +0800

    update to binary matrix

--- feature_detection.py
+++ feature_detection.py
@@ -3,6 +3,7 @@ import essentia.standard
 from essentia.standard import *
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.signal
 
 def read_audio(filename, sample_rate = 44100):
     """read audio file into a list"""
@@ -99,14 +100,59 @@ def enhence_sdm(sdm):
 
     return enhenced_mat
 
-def detect_repetition(sdm):
+def detect_repetition(sdm, diagonal_num = 20, thres_rate = 0.2):
     """detect repetition"""
     length = len(sdm)
-    dig = np.zeros([length, 1])
+    dig_mean = np.zeros(length)
     for i in range(0, length):
-        dig[i] = np.sum(np.diag(sdm, -i)) / (length - i)
+        dig_mean[i] = np.sum(np.diag(sdm, -i)) / (length - i)
 
-    return dig
+    # using a FIR filter to smooth mean of diagonals
+    B = np.ones(50) / 50
+    dig_lp = scipy.signal.lfilter(B, 1, dig_mean)
+    dig = dig_mean - dig_lp
+
+    # calculate the smoothed differential of diagonals
+    B = np.array([1, 0, -1])
+    dig_smooth_diiiferentia = scipy.signal.lfilter(B, 1 ,dig)
+
+    # index where the smoothed differential of diagonals from negative to positive
+    minima = np.array([])
+    minima_indeces = np.array([], dtype = int)
+    for i in range(0, len(dig_smooth_diiiferentia) - 1):
+        if dig_smooth_diiiferentia[i] < 0 and dig_smooth_diiiferentia[i + 1] > 0:
+            minima_indeces = np.append(minima_indeces, i)
+            minima = np.append(minima, dig_smooth_diiiferentia[i])
+
+    # delete max value
+    if len(minima) > diagonal_num:
+        add = np.where(minima == max(minima))
+        add = add[0 : len(minima) - diagonal_num]
+        minima = np.delete(minima, add)
+        minima_indeces = np.delete(minima_indeces, add)
+
+    # calculate a threshold
+    long_vector = np.array([])
+    for index in minima_indeces:
+        long_vector = np.append(long_vector, np.diag(-index))
+
+    all_len = len(long_vector)
+    long_vector = np.sort(long_vector)
+    threshold = long_vector[int(round(thres_rate * all_len))]
+
+    # calculate a binary matrix
+    binary_matrix = np.zeros([length, length])
+
+    for index in minima_indeces:
+        temp = np.diag(sdm, -index)
+        for j in range(0, len(temp)):
+            if temp[j] > threshold:
+                binary_matrix[index + j, j] = 1
+
+
+
+
+    return dig_mean, dig, dig_lp, dig_smooth_diiiferentia
 
 
 # extract audio feature
@@ -125,11 +171,17 @@ enhenced_mat = enhence_sdm(sdm_chroma)
 # sum the mfcc self-distance matrix and enhenced chroma self-distance matrix
 sdm_new = enhenced_mat + sdm_mfcc
 
-dig = detect_repetition(enhenced_mat)
+dig_mean, dig, dig_lp, dig_smooth_diiiferentia = detect_repetition(enhenced_mat)
 
 # plt.matshow(enhenced_mat, cmap=plt.cm.gray)
 # plt.matshow(sdm_mfcc, cmap=plt.cm.gray)
 # plt.matshow(sdm_new, cmap=plt.cm.gray)
+
+
+plt.plot(dig_mean)
 plt.plot(dig)
+plt.plot(dig_lp)
+plt.plot(dig_smooth_diiiferentia)
+
 plt.show()
-print "123"
+print dig_smooth_diiiferentia
